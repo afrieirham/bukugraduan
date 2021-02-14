39e2312d6028976393bc3bd9104d84781a42e735
afrie <itsafrie@gmail.com>
2021-02-14 19:16:47 +0800

    Update date format in dashboard

--- components/BooklistTableRow.js
+++ components/BooklistTableRow.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import { Trash } from 'react-feather'
-import { format, parseISO } from 'date-fns'
+import { formatRelative, parseISO } from 'date-fns'
 import { Box, IconButton, Switch } from '@chakra-ui/react'
 
 import { Td } from './Table'
-import { useAuth } from '@/utils/auth'
 
 function BooklistTableRow({ title, price, isSold, createdAt }) {
   return (
@@ -12,9 +11,9 @@ function BooklistTableRow({ title, price, isSold, createdAt }) {
       <Td>{title}</Td>
       <Td>{price}</Td>
       <Td>
-        <Switch colorScheme='green' />
+        <Switch colorScheme='green' isChecked={isSold} />
       </Td>
-      <Td>{format(parseISO(createdAt), 'd/MM/yyyy')}</Td>
+      <Td>{formatRelative(parseISO(createdAt), new Date())}</Td>
       <Td>
         <IconButton variant='ghost'>
           <Trash size='15' />
