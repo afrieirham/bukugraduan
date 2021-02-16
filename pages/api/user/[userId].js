c25a9165bb2a979a2fcb895f42448bbb3157b973
afrie <itsafrie@gmail.com>
2021-02-17 03:21:20 +0800

    Make /api/user/[userId] public

--- pages/api/user/[userId].js
+++ pages/api/user/[userId].js
@@ -1,9 +1,7 @@
 import { getUserDetails } from '@/utils/db-admin'
-import { auth } from '@/lib/firebase-admin'
 
 export default async (req, res) => {
   try {
-    await auth.verifyIdToken(req.headers.token)
     const { user } = await getUserDetails(req.query.userId)
     res.status(200).json({ user })
   } catch (error) {
