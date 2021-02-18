dc4dff06a13268b43d0086e8b826bea750fe84a3
afrie <itsafrie@gmail.com>
2021-02-18 23:46:58 +0800

    Add feedback link to google form

--- components/Footer.js
+++ components/Footer.js
@@ -5,33 +5,34 @@ import { BookOpen } from 'react-feather'
 
 function Footer() {
   return (
-    <Stack
+    <Flex
+      direction='column'
+      width='full'
       justifyContent='center'
       alignItems='center'
-      p={16}
-      isInline
-      spacing={4}
       color='gray.500'
       fontSize='xs'
       fontWeight='semibold'
+      p={16}
     >
-      <Flex justifyContent='center' alignItems='center'>
-        <BookOpen size='20' />
-        <Text ml={2}>Buku Graduan &copy; {format(new Date(), 'yyyy')}</Text>
-      </Flex>
-      <Link
-        href='https://www.freeprivacypolicy.com/live/d70c76da-faca-422e-be8e-5d61c7e3e843'
-        isExternal
-      >
-        Privacy
-      </Link>
-      <Link href='https://instagram.com/bukugraduan' isExternal>
-        Instagram
-      </Link>
-      <Link href='mailto:afrieirham@outlook.com' isExternal>
-        Contact
-      </Link>
-    </Stack>
+      <Stack justifyContent='center' alignItems='center' isInline spacing={4}>
+        <Link
+          href='https://www.freeprivacypolicy.com/live/d70c76da-faca-422e-be8e-5d61c7e3e843'
+          isExternal
+        >
+          Privacy
+        </Link>
+        <Link href='https://forms.gle/hWE9cZXRViMAtFBu8' isExternal>
+          Feedback
+        </Link>
+        <Link href='mailto:afrieirham@outlook.com' isExternal>
+          Contact
+        </Link>
+        <Link href='https://instagram.com/bukugraduan' isExternal>
+          Instagram
+        </Link>
+      </Stack>
+    </Flex>
   )
 }
 
